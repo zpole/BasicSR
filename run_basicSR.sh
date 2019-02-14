2610797293e3335ecefee3e4deded8ea63abea7d
zpole <zhi951206@gmail.com>
2019-02-14 04:47:07 +0000

    chang option

--- run_basicSR.sh
+++ run_basicSR.sh
@@ -36,3 +36,4 @@ export DATASET_DIR=${TMP}/datasets/
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 cd /home/${STUDENT_ID}/BasicSR/codes
 python test.py -opt options/test/test_SRGAN.json
+python test.py -opt options/test/test_sr.json
